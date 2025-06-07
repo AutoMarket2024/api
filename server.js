27370b9b146b6b5a96e956f98b03e32c8d1ecc23
arnaldo89 <arnaldoassis89@gmail.com>
2025-06-07 22:15:04 +0100

    add logica na api carro

--- server.js
+++ server.js
@@ -27,6 +27,25 @@ app.post("/usuario", async (req, res) => {
   }
 });
 
+
+// metodo post para criar carro
+app.post("/carro", async (req, res) => {
+  try {
+    const cars = await prisma.carro.create({
+      data: {
+        matricula: req.body.matricula,
+        marca: req.body.marca,
+        modelo: req.body.modelo,
+        cor: req.body.cor,
+        cilindrada: parseInt(req.body.cilindrada),
+      },
+    });
+    res.status(201).json(cars);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 // metodo get para listar usuario
 app.get("/usuario", async (req, res) => {
   try {
@@ -55,6 +74,34 @@ app.get("/usuario", async (req, res) => {
   }
 });
 
+// metodo get para listar carro
+app.get("/carro", async (req, res) => {
+  try {
+    let cars = [];
+    
+    // Check if there are query parameters for filtering
+    if (Object.keys(req.query).length > 0) {
+      const whereClause = {};
+      
+      // Build where clause based on query parameters
+      if (req.query.matricula) whereClause.matricula = req.query.matricula;
+      if (req.query.marca) whereClause.marca = req.query.marca;
+      if (req.query.modelo) whereClause.modelo = req.query.modelo;
+      if (req.query.cor) whereClause.cor = req.query.cor;
+      if (req.query.cilindrada) whereClause.cilindrada = req.query.cilindrada;
+      cars = await prisma.carro.findMany({
+        where: whereClause,
+      });
+    } else {
+      cars = await prisma.carro.findMany();
+    }
+    
+    res.status(200).json(cars);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 // metodo put para editar usuario
 app.put("/usuario/:id", async (req, res) => {
   try {
@@ -77,10 +124,46 @@ app.put("/usuario/:id", async (req, res) => {
   }
 });
 
+// metodo put para editar carro
+app.put("/carro/:id", async (req, res) => {
+  try {
+    const updatedCar = await prisma.carro.update({
+      where: {
+        id: req.params.id,
+      },
+      data: {
+        matricula: req.body.matricula,
+        marca: req.body.marca,
+        modelo: req.body.modelo,
+        cor: req.body.cor,
+        cilindrada: parseInt(req.body.cilindrada)
+      },
+    });
+    res.status(200).json(updatedCar);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+
 // metodo delete para deletar usuario
 app.delete("/usuario/:id", async (req, res) => {
   try {
     await prisma.user.delete({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// metodo delete para deletar carro
+app.delete("/carro/:id", async (req, res) => {
+  try {
+    await prisma.carro.delete({
       where: {
         id: req.params.id,
       },
